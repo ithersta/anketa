535478b423f1b21f7f4e1e4c3218dd364b09bf0f
Владислав Шиляев <schilyaev.vlad@gmail.com>
2024-01-23 13:58:59 +0300

    Fix CSP (#16)

--- frontend/src/routes/oauth/yandex/LoginWithYandex.svelte
+++ frontend/src/routes/oauth/yandex/LoginWithYandex.svelte
@@ -1,42 +1,40 @@
 <script lang="ts">
     import { mode } from 'mode-watcher';
     import { onMount } from "svelte";
-    import { PUBLIC_YANDEX_OAUTH_CLIENT_ID, PUBLIC_BASE_URL } from "$env/static/public";
+    import { PUBLIC_BASE_URL, PUBLIC_YANDEX_OAUTH_CLIENT_ID } from "$env/static/public";
     import { page } from "$app/stores";
 
     onMount(() => {
-        window.onload = function () {
-            window.YaAuthSuggest.init({
-                    client_id: PUBLIC_YANDEX_OAUTH_CLIENT_ID,
-                    response_type: 'token',
-                    redirect_uri: `${PUBLIC_BASE_URL}/oauth/yandex`
-                },
-                PUBLIC_BASE_URL, {
-                    view: 'button',
-                    parentId: 'yandex-button',
-                    buttonView: 'main',
-                    buttonTheme: $mode,
-                    buttonSize: 'xxl',
-                    buttonBorderRadius: 16
+        window.YaAuthSuggest.init({
+                client_id: PUBLIC_YANDEX_OAUTH_CLIENT_ID,
+                response_type: 'token',
+                redirect_uri: `${PUBLIC_BASE_URL}/oauth/yandex`
+            },
+            PUBLIC_BASE_URL, {
+                view: 'button',
+                parentId: 'yandex-button',
+                buttonView: 'main',
+                buttonTheme: $mode,
+                buttonSize: 'xxl',
+                buttonBorderRadius: 16
+            }
+        )
+            .then(function (result: any) {
+                return result.handler()
+            })
+            .then(async function (data: { access_token: string }) {
+                let response = await fetch("oauth/yandex", {
+                    method: "POST",
+                    body: JSON.stringify({ token: data.access_token }),
+                    headers: { "Content-Type": "application/json" },
+                })
+                if (response.ok) {
+                    location.assign($page.url.searchParams.get("next") ?? "/")
                 }
-            )
-                .then(function (result: any) {
-                    return result.handler()
-                })
-                .then(async function (data: {access_token: string}) {
-                    let response = await fetch("oauth/yandex", {
-                        method: "POST",
-                        body: JSON.stringify({ token: data.access_token }),
-                        headers: { "Content-Type": "application/json" },
-                    })
-                    if (response.ok) {
-                        location.assign($page.url.searchParams.get("next") ?? "/")
-                    }
-                })
-                .catch(function (error: any) {
-                    console.log(error)
-                })
-        }
+            })
+            .catch(function (error: any) {
+                console.log(error)
+            })
     })
 </script>
 
