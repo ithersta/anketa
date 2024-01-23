a9d1b1353feea299cf8b8855356e576160bfef4b
Владислав Шиляев <schilyaev.vlad@gmail.com>
2024-01-23 12:15:16 +0300

    Add default email

--- backend/src/main/resources/db/migration/V3_1__add_user_attrs.sql
+++ backend/src/main/resources/db/migration/V3_1__add_user_attrs.sql
@@ -1,5 +1,5 @@
 alter table users
-    add column email varchar not null unique default 'ithersta@yandex.ru';
+    add column email varchar not null unique default substr(uuid_generate_v4()::text,1,15);
 
 alter table users
     add column display_name varchar not null default 'ithersta';
