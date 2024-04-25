50c8b0ff875e8b42dfcf95de1f170720f262f442
Владислав Шиляев <schilyaev.vlad@gmail.com>
2024-04-25 16:44:47 +0300

    Change prompt

--- backend/src/main/kotlin/com/ithersta/anketa/gpt/SummarizationService.kt
+++ backend/src/main/kotlin/com/ithersta/anketa/gpt/SummarizationService.kt
@@ -3,13 +3,23 @@ package com.ithersta.anketa.gpt
 import kotlinx.coroutines.delay
 import org.springframework.stereotype.Service
 
+private const val SystemMessage = """Проанализируй ответы на вопрос анкеты. На основании этого анализа сделай вывод о тональности всего обсуждения: позитивная, негативная, нейтральная. После этого перескажи тезисы респондентов. Игнорируй непонятные ответы.
+
+Формат твоего ответа:
+```
+Тональность: ...
+Тезисы:
+...
+Можно сделать вывод...
+```"""
+
 @Service
 class SummarizationService(
     private val gptApi: YandexGptApi,
 ) {
     suspend fun summarize(question: String, answers: List<String>): String {
         val id = gptApi.sendAsync(gptApi.createRequest(
-            systemMessage = "Проанализируй ответы на вопрос анкеты. На основании этого анализа сделай вывод о тональности всех ответов в целом: позитивная, негативная, нейтральная. После этого, если они есть, перечисли список пожеланий респондентов.",
+            systemMessage = SystemMessage,
             userMessage = "Вопрос:\n$question\n\nОтветы:\n${answers.joinToString("\n")}"
         ))
         for (i in 0..30) {
